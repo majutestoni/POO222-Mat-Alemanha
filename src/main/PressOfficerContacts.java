68db3fd0c4b83e365a69585dc0ae75663beed7d8
jiribeiro <jonathan_jir@hotmail.com>
2022-11-20 18:19:23 -0300

    [feature/joni-1] criação classes PressOfficerContacts & TechnicalCommittee

--- src/main/PressOfficerContacts.java
+++ src/main/PressOfficerContacts.java
@@ -5,13 +5,15 @@ import java.time.LocalDate;
 public class PressOfficerContacts extends Person {
 
 	private String email;
-	private String telephones[] = new String[2];
-	
-	public PressOfficerContacts(String name, LocalDate birthdayDate, String nickname, String email,
-			String[] telephones) {
+	private String tel1;
+	private String tel2;
+
+	public PressOfficerContacts(String name, LocalDate birthdayDate, String nickname, String email, String tel1,
+			String tel2) {
 		super(name, birthdayDate, nickname);
-		this.setEmail(email);
-		this.telephones = telephones;
+		this.email = email;
+		this.tel1 = tel1;
+		this.tel2 = tel2;
 	}
 
 	public String getEmail() {
@@ -26,12 +28,28 @@ public class PressOfficerContacts extends Person {
 		}
 	}
 
-	public String[] getTelephones() {
-		return telephones;
+	public String getTel1() {
+		return tel1;
 	}
 
-	public void setTelephones(String[] telephones) {
-		this.telephones = telephones;
+	public void setTel1(String tel1) {
+		if (tel1 != null && !tel1.isBlank()) {
+			this.tel1 = tel1;
+		} else {
+			throw new IllegalArgumentException("Invalid number");
+		}
+	}
+
+	public String getTel2() {
+		return tel2;
+	}
+
+	public void setTel2(String tel2) {
+		if (tel2 != null && !tel2.isBlank()) {
+			this.tel2 = tel2;
+		} else {
+			throw new IllegalArgumentException("Invalid number");
+		}
 	}
 	
 	
